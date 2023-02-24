d85e916c2f9f4ca1b410347e76d577600f4df9d4
Leon <Leon@MacBook-Pro.lan>
2023-02-24 12:08:21 -0500

    img attempt

---
+++ app.js
@@ -0,0 +1,32 @@
+$(() => {
+	openAboutMe = () => {
+        $('.imgAndBackground').css('display', 'none')
+        $('.aboutMe').css('display', 'block')
+    }
+
+    openProjects = () => {
+        $('.imgAndBackground').css('display', 'none')
+        $('.projects').css('display', 'block')
+    }
+    openResume = () => {
+        $('.imgAndBackground').css('display', 'none')
+        $('.resume').css('display', 'block')
+    }
+    $('#About').on('click', openAboutMe)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
