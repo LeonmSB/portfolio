7795800ff199da7d33adf6fb5b9b0701f7ccd6d3
Leon <Leon@MacBook-Pro.lan>
2023-02-28 14:54:10 -0500

    next steps

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
 $(() => {
-	openAboutMe = () => {
+	const openAboutMe = () => {
         $('.imgAndBackground').css('display', 'none')
         $('.projectsContainer').css('display', 'none')
         $('.resume').css('display', 'none')
         $('.aboutMe').css('display', 'block')
         $('#Home').css('display', 'block')
+        // $('#About').css('background', 'lightblue')
     }
 
-    openProjects = () => {
+    const openProjects = () => {
+        // unBlue()
         $('.aboutMe').css('display', 'none')
         $('.imgAndBackground').css('display', 'none')
         $('.resume').css('display', 'none')
         $('.projectsContainer').css('display', 'flex')
         $('.projectsContainer').css('justify-content', 'space-around')
         $('#Home').css('display', 'block')
+        // $('#About').css('background', 'lightblue')
         
     }
-    openResume = () => {
+    const openResume = () => {
+        // unBlue()
         $('.aboutMe').css('display', 'none')
         $('.imgAndBackground').css('display', 'none')
         $('.projectsContainer').css('display', 'none')
         $('.resume').css('display', 'block')
         $('#Home').css('display', 'block')
+        // $('#Resume').css( 'background', 'lightblue')
 
     }
 
-    returnHome = () => {
+    const returnHome = () => {
+        // unBlue()
         $('.projectsContainer').css('display', 'none')
         $('.aboutMe').css('display', 'none')
         $('.resume').css('display', 'none')
@@ -35,12 +41,33 @@ $(() => {
 
 
     }
-
-
+    darkMode = false
+    const toggleDarkMode = () => {
+            if (darkMode == false) {
+                $('body').css('background', 'gray')
+                $('#darkmode').css('color', 'yellow')
+                darkMode = true
+            } else {
+                $('body').css('background', 'rgb(151, 181, 191)')
+                $('#darkmode').css('color', 'gray')
+                darkMode = false
+            }
+    }
+    // const unBlue = () => {
+    //     $('.topdiv').css( 'background', 'rgb(151, 181, 191)')
+    //     $('.topdiv').hover(function() {
+    //         $(this).css( 'background-color', 'lightblue')
+    //     }, function(){
+    //         $(this).css("background-color", "rgb(151, 181, 191)");
+    //       })
+    //     }
+    // unBlue()
     $('#About').on('click', openAboutMe)
     $('#Projects').on('click', openProjects)
     $('#Resume').on('click', openResume)
     $('#Home').on('click', returnHome)
+    $('#Home').on('click', returnHome)
+    $('#darkmode').on('click', toggleDarkMode)
 
     let currentProjectIndex = 0
 
